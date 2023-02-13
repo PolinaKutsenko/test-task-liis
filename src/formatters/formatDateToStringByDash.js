4c134e6a2dd0c0eac66020f91f05ead123089a02
Polina Kutsenko <103639323+PolinaKutsenko@users.noreply.github.com>
2023-02-13 15:27:41 +0300

    Update formatDateToStringByDash.js

--- src/formatters/formatDateToStringByDash.js
+++ src/formatters/formatDateToStringByDash.js
@@ -1,7 +1,7 @@
 const formatDateToStringByDash = (date) => {
   const year = date.getFullYear();
   const month = (date.getMonth() + 1) > 10 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`;
-  const day = date.getDate() > 10 ? date.getDate() : `0${date.getDate()}`;
+  const day = date.getDate() >= 10 ? date.getDate() : `0${date.getDate()}`;
   return `${year}-${month}-${day}`;
 };
 
